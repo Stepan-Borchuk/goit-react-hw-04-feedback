91fd441c1a61a7993b8f545d1c7285133a60ac51
Stepan <91005125+Stepan-Borchuk@users.noreply.github.com>
2022-07-10 18:37:51 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,67 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Box } from './box/Box';
+import { Options } from './butons/Buttons';
+import { Statistics } from './statistics/Statistics';
+import { Title } from './App.styled';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleFeedback = value => {
+    this.setState(prevState => {
+      return { [value]: prevState[value] + 1 };
+    });
+  };
+
+  totalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  positiveFeedback = () => {
+    const total = this.totalFeedback();
+    const { good } = this.state;
+    return total !== 0 ? Math.round((good * 100) / total) : 0;
+  };
+
+  render() {
+    const buttonsArray = Object.keys(this.state);
+    const statsArray = Object.entries(this.state);
+    const total = this.totalFeedback();
+    const value = this.positiveFeedback();
+    return (
+      <Box
+        as="div"
+        width="75%"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="column"
+        color="white"
+        ml="auto"
+        mr="auto"
+        bg="background"
+        borderRadius="normal"
+        boxShadow="4px 11px 49px 1px #d7dead"
+      >
+        <Box as="section">
+          <Title>Please leave feedback</Title>
+          <Options options={buttonsArray} onFeedback={this.handleFeedback} />
+        </Box>
+
+        <Box as="section" display="flex" flexDirection="column" width="50%">
+          <h2>Statistics</h2>
+          {total === 0 ? (
+            <h3>No feedback given</h3>
+          ) : (
+            <Statistics total={total} good={value} statsArray={statsArray} />
+          )}
+        </Box>
+      </Box>
+    );
+  }
+}
