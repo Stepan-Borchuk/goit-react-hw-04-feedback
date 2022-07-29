cfc2365a920c40083fe0cef3cc7bffd6709ff76a
Stepan <91005125+Stepan-Borchuk@users.noreply.github.com>
2022-07-29 23:51:43 +0300

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,70 +1,87 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Box } from './box/Box';
 import { FeedbackOptions } from './butons/Buttons';
 import { Statistics } from './statistics/Statistics';
 import { Title } from './App.styled';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleFeedback = value => {
+    switch (value) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+
+      default:
+        return;
+    }
   };
 
-  handleFeedback = value => {
-    this.setState(prevState => {
-      return { [value]: prevState[value] + 1 };
-    });
-  };
-
-  totalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const totalFeedback = () => {
     return good + neutral + bad;
   };
 
-  positiveFeedback = () => {
-    const total = this.totalFeedback();
-    const { good } = this.state;
+  const positiveFeedback = () => {
+    const total = totalFeedback();
     return total !== 0 ? Math.round((good * 100) / total) : 0;
   };
 
-  render() {
-    const buttonsArray = Object.keys(this.state);
-    const statsArray = Object.entries(this.state);
-    const total = this.totalFeedback();
-    const value = this.positiveFeedback();
-    return (
-      <Box
-        as="div"
-        width="75%"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        flexDirection="column"
-        color="white"
-        ml="auto"
-        mr="auto"
-        bg="background"
-        borderRadius="normal"
-        boxShadow="4px 11px 49px 1px #d7dead"
-      >
-        <Box as="section">
-          <Title>Please leave feedback</Title>
-          <FeedbackOptions
-            options={buttonsArray}
-            onLeaveFeedback={this.handleFeedback}
-          />
-        </Box>
+  const buttonsArray = ['good', 'neutral', 'bad'];
+  const statsArray = [
+    ['good', good],
+    ['neutral', neutral],
+    ['bad', bad],
+  ];
 
-        <Box as="section" display="flex" flexDirection="column" width="50%">
-          <h2>Statistics</h2>
-          {total === 0 ? (
-            <h3>No feedback given</h3>
-          ) : (
-            <Statistics total={total} good={value} statsArray={statsArray} />
-          )}
-        </Box>
+  return (
+    <Box
+      as="div"
+      width="75%"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      flexDirection="column"
+      color="white"
+      ml="auto"
+      mr="auto"
+      bg="background"
+      borderRadius="normal"
+      boxShadow="4px 11px 49px 1px #d7dead"
+    >
+      <Box as="section">
+        <Title>Please leave feedback</Title>
+        <FeedbackOptions
+          options={buttonsArray}
+          onLeaveFeedback={handleFeedback}
+        />
       </Box>
-    );
-  }
+
+      <Box as="section" display="flex" flexDirection="column" width="50%">
+        <h2>Statistics</h2>
+
+        {totalFeedback() === 0 ? (
+          <h3>No feedback given</h3>
+        ) : (
+          <Statistics
+            total={totalFeedback()}
+            good={positiveFeedback()}
+            statsArray={statsArray}
+          />
+        )}
+      </Box>
+    </Box>
+  );
 }
+
+// Language: javascript
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+
 import './index.css';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './components/theme';
+import App from 'components/App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot( document.getElementById( 'root' ) )
+    .render(
+        <React.StrictMode>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>
-);
+    );
